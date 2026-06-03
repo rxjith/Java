7474d281517ea2dc6b5b25bb70412ed7d3b577a7
rxjith <rojith.jinenth@gmail.com>
2026-06-03 19:04:04 +0530

    Comments added

--- UserInput.java
+++ UserInput.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
+
 public class UserInput {
     public static void main(String[] args) {
-        String name;
-        try (Scanner input = new Scanner(System.in)) {
-            System.out.print("Enter your name: ");
-                name = input.nextLine();
-        }
-        System.out.println("Hello, " + name + "!");
+        String name;                                        // String declaration
+        try (Scanner input = new Scanner(System.in)) {      // Try-with-Resources method
+            System.out.print("Enter your name: ");          // Prompting...
+                name = input.nextLine();                    // User input
+        }           
+        System.out.println("Hello, " + name + "!");         // Printing greeting
     }    
 }
