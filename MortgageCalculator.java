12e6be827d745e8977748b450db37920a1642ac8
rxjith <rojith.jinenth@gmail.com>
2026-06-04 12:44:00 +0530

    Refactored magic numbers to constants

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -5,6 +5,8 @@ public class MortgageCalculator {
     public static void main(String[] args) {
         double principal, rate;
         int time;
+        final byte MONTHS_IN_A_YEAR = 12;
+        final byte PERCENT = 100;
         System.out.println("Welcome to my mortgage calculator!");
         try (Scanner input = new Scanner(System.in)) {
             System.out.print("Enter principal amount: ");
@@ -16,8 +18,8 @@ public class MortgageCalculator {
             System.out.print("Enter time (in years): ");
             time = input.nextInt();
 
-            double r = rate / 12 / 100;
-            int n = time * 12;
+            double r = rate / MONTHS_IN_A_YEAR / PERCENT;
+            int n = time * MONTHS_IN_A_YEAR;
 
             NumberFormat currency = NumberFormat.getCurrencyInstance();
             double emi = principal * (r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1);
