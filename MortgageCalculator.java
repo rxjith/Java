39a0487760051039ab6ced433700cafd56acaa1b
rxjith <rojith.jinenth@gmail.com>
2026-06-04 14:26:03 +0530

    UX Improvements

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -9,14 +9,41 @@ public class MortgageCalculator {
         final byte PERCENT = 100;
         System.out.println("Welcome to my mortgage calculator!");
         try (Scanner input = new Scanner(System.in)) {
-            System.out.print("Enter principal amount: ");
-            principal = input.nextDouble();
-            
-            System.out.print("Enter annual interest: ");
-            rate = input.nextDouble();
+            // Prinicpal validation:
+            while (true) {
+                System.out.print("Enter principal amount: ");
+                principal = input.nextDouble();
 
-            System.out.print("Enter time (in years): ");
-            time = input.nextInt();
+                if (!(principal > 1_000) || !(principal <= 1_000_000)) {
+                    System.out.println("Please enter an amount from $1K - $1M!");
+                } else {
+                    break;
+                }
+            }
+
+            // Period validation:
+            while (true) {    
+                System.out.print("Enter annual interest: ");
+                rate = input.nextDouble();
+
+                if (!(rate > 0) || !(rate <= 30)) {
+                    System.out.println("Please enter an amount greater than 0 and less than or equal to 30!");
+                } else {
+                    break;
+                }
+            }
+
+            // Rate validation:
+            while (true) {
+                System.out.print("Enter time (in years): ");
+                time = input.nextInt();
+
+                if (!(time >= 1) || !(time <= 30)) {
+                    System.out.println("Please enter a period between 1 and 30 years!");
+                } else {
+                    break;
+                }
+            }
 
             double r = rate / MONTHS_IN_A_YEAR / PERCENT;
             int n = time * MONTHS_IN_A_YEAR;
