b1f937b22f806124077d871a54d575cd3c584dad
rxjith <rojith.jinenth@gmail.com>
2026-06-04 18:49:17 +0530

    Finalized with methods

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -2,55 +2,46 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class MortgageCalculator {
+    final static byte MONTHS_IN_A_YEAR = 12;
+    final static byte PERCENT = 100;
+
     public static void main(String[] args) {
-        double principal, rate;
-        int time;
-        final byte MONTHS_IN_A_YEAR = 12;
-        final byte PERCENT = 100;
         System.out.println("Welcome to my mortgage calculator!");
+
         try (Scanner input = new Scanner(System.in)) {
-            // Prinicpal validation:
-            while (true) {
-                System.out.print("Enter principal amount: ");
-                principal = input.nextDouble();
 
-                if (!(principal > 1_000) || !(principal <= 1_000_000)) {
-                    System.out.println("Please enter an amount from $1K - $1M!");
-                } else {
-                    break;
-                }
-            }
+            double principal = readNumber(input, "Enter a principal amount from $1K - $1M: ", 1_000, 1_000_000);
 
-            // Period validation:
-            while (true) {    
-                System.out.print("Enter annual interest: ");
-                rate = input.nextDouble();
+            double rate = readNumber(input, "Enter rate of interest (0-30): ", 0, 30);
 
-                if (!(rate > 0) || !(rate <= 30)) {
-                    System.out.println("Please enter an amount greater than 0 and less than or equal to 30!");
-                } else {
-                    break;
-                }
-            }
+            int time = (int) readNumber(input, "Enter tenure of mortgage (1-30): ", 1, 30);
 
-            // Rate validation:
-            while (true) {
-                System.out.print("Enter time (in years): ");
-                time = input.nextInt();
-
-                if (!(time >= 1) || !(time <= 30)) {
-                    System.out.println("Please enter a period between 1 and 30 years!");
-                } else {
-                    break;
-                }
-            }
-
-            double r = rate / MONTHS_IN_A_YEAR / PERCENT;
-            int n = time * MONTHS_IN_A_YEAR;
-
-            NumberFormat currency = NumberFormat.getCurrencyInstance();
-            double emi = principal * (r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1);
-            System.out.printf("Your mortgage per month is: %s\n", currency.format(emi));
+            double mortgage = calculateMortgage(principal, rate, time);
+            printMortgage(mortgage);
         }
+    }
+
+    static double readNumber(Scanner input, String prompt, double min, double max) {
+        while (true) {
+            System.out.print(prompt);
+            double value = input.nextDouble();
+
+            if (value > min && value <= max)
+                return value;
+
+            System.out.printf("Invalid! Enter a value between %.0f and %.0f%n", min, max);
+        }
+    }
+
+    static double calculateMortgage(double principal, double annualInterest, int time) {
+        double r = annualInterest / MONTHS_IN_A_YEAR / PERCENT;
+        int n = time * MONTHS_IN_A_YEAR;
+        return principal * (r * Math.pow(1 + r, n)) / (Math.pow(1+r, n) - 1);
+    }
+
+    static void printMortgage(double mortgage) {
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+
+        System.out.println("Your mortgage per month is: " + currency.format(mortgage));
     }
 }
