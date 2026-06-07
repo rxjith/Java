49d92a37b0cd0573c39814dcdf12f06fc827358d
rxjith <rojith.jinenth@gmail.com>
2026-06-07 18:08:47 +0530

    Linear Search

--- LinearSearch.java
+++ LinearSearch.java
@@ -3,37 +3,43 @@ import java.util.Scanner;
 public class LinearSearch {
     public static void main(String[] args) {
         
-        int[] array; boolean found = false; int position = 0;
-        Scanner scanner = new Scanner(System.in);
+        int[] array;                                                                // Instantiate array
+        Scanner scanner = new Scanner(System.in);                                   // Instantiate scanner
         
-        System.out.print("Enter number of elements in the array: ");
-        int size = scanner.nextInt(); 
-        scanner.nextLine();
+        System.out.print("Enter number of elements in the array: ");                // Prompt user for size of array
+        int size = scanner.nextInt();                                               // User enters size
+        scanner.nextLine();                                                         // Clear buffer
 
-        array = new int[size];
+        array = new int[size];                                                      // Define array with size
 
         for (int i = 0; i < array.length; i++) {
-            System.out.printf("Enter element %d: ", i + 1);
-            array[i] = scanner.nextInt();
-            scanner.nextLine();
+            System.out.printf("Enter element %d: ", i + 1);                         // Prompt user for each element
+            array[i] = scanner.nextInt();                                           // Enter each element
+            scanner.nextLine();                                                     // Clear buffer
         }
 
-        System.out.print("Enter the element you wanna search for: ");
-        int key = scanner.nextInt();
-        scanner.nextLine();
-        scanner.close();
+        System.out.print("Enter the element you wanna search for: ");               // Prompt user for key
+        int key = scanner.nextInt();                                                // User enters key
+        scanner.nextLine();                                                         // Clear buffer
+        scanner.close();                                                            // Close scanner
+        
+        LinearSearchAlgo(key, array);                                               // Call Linear Search Method
+    }
+
+    static void LinearSearchAlgo(int key, int[] array) {
+        boolean found = false;
+        int position = 1;                                                           // Local variables initialization
 
         for (int number : array) {
-            position++;
             if (key == number) {
-                found = true;
+                found = true;                                                       // Linear Search Implementation
                 break;
-            }
+            } position++;
         }
         
         if (found)
-            System.out.printf("%d was found in position %d\n", key, position);
+            System.out.printf("%d was found at positon %d!\n", key, position);      // Display appropriate message
         else
-            System.out.printf("%d was not found in the array!\n", key);
+            System.out.printf("%d was not found within the array.\n", key);
     }
 }
