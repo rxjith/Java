972bd8baebb3746ec73edc156a3dbd925bf81dfd
rxjith <rojith.jinenth@gmail.com>
2026-07-28 13:50:55 +0530

    Leap Year Checker

---
+++ labWork/leapYearChecker.java
@@ -0,0 +1,27 @@
+package labWork;
+import java.util.Scanner;
+
+public class leapYearChecker {
+	
+	public static void main(String[] args) {
+		// Leap year checker
+		
+		try (Scanner scanner = new Scanner(System.in)) {
+			
+			System.out.print("Enter a year: ");
+			int year = scanner.nextInt();
+			
+			boolean isLeap = false;
+			
+			if (((year % 4 == 0) && (year % 100 != 0)) || year % 4 == 0) {
+				isLeap = true;
+			}
+			
+			if (isLeap) {
+				System.out.println(year + " is a LEAP year.");
+			} else {
+				System.out.println(year + " is NOT a LEAP year.");
+			}
+		}
+	}
+}
