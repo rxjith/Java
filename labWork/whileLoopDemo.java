7486f68f8d18b1e564dc41ec75b2e1507d78a6ab
rxjith <rojith.jinenth@gmail.com>
2026-07-28 12:53:17 +0530

    Lab Work Initial Commit

---
+++ labWork/whileLoopDemo.java
@@ -0,0 +1,18 @@
+package labWork;
+
+public class whileLoopDemo {
+
+	public static void main(String[] args) {
+		// Program that prints all even #s between 1 & 20 using a while loop
+		
+		int i = 2;
+		
+		System.out.println("Even numbers from 1-20: ");
+		while (i <= 20) {
+			System.out.print(i + " ");
+			i+=2;
+		}
+
+	}
+
+}
